b0ee79347bb7ab015261c487f5ab7de3be456afa
Jonathan Lee <joncslee@gmail.com>
2013-09-02 10:01:51 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studiology::Application.config.secret_key_base = 'd0cc6b11ba4d0527bddc9e6c036153da23a1025e2755b9b866217cd973ad45e0d2b866e35a93a4fe14ba9be704c5506b9f5f58fbc479153824d1a654882ef42e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Studiology::Application.config.session_store :cookie_store, key: '_studiology_session'
