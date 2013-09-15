19c7f4a4a02cba84ec98ce7c16c3754bc994e48f
Jonathan Lee <joncslee@gmail.com>
2013-09-15 10:00:05 -0400

    set after signin path to be user's studio page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(user) 
+    "/#{user.username}"
+  end 
+
   protected
 
   def configure_permitted_parameters
